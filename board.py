e6a1e6c5e12f775b37e8b501c3a176e7da0929f6
hamza-soilihi <soilihihamza2@gmail.com>
2024-12-06 22:26:24 +0100

    ajout mise a jour board

--- board.py
+++ board.py
@@ -3,7 +3,7 @@
 def create_board(): return [[" "] * 3 for _ in range(3)]
 
 def display_board(board):
-    symbols = {"X": "❌", "O": "⭕", " ": "⬜"}
-    print("    0   1   2\n  +---+---+---+")
+    symbols = {"X": "❌", "O": "⭕", " ": "⬜ "}
+    print("    0   1   2\n  +-----+-----+-----+")
     for i, row in enumerate(board):
-        print(f"{i} | {' | '.join(symbols[cell] for cell in row)} |\n  +---+---+---+")
+        print(f"{i} | {' | '.join(symbols[cell] for cell in row)} |\n  +-----+-----+-----+")
